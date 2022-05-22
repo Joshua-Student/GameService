2b3b10f29d65e8e0305e3bbef640efa02aaa30df
Joshua-Student <student.shea@gmail.com>
2022-05-22 19:31:07 -0400

    Cleaned up some comments.

--- GameService/Controllers/GameController.cs
+++ GameService/Controllers/GameController.cs
@@ -29,91 +29,19 @@ namespace GameService.Controllers
             return View();
         }
 
-        //[HttpGet("{id}")]
-        //// GET: GameController/Details/5
-        //public ActionResult Details(int id)
-        //{
-        //    ReturnObject ro = new ReturnObject
-        //    {
-        //        Valid = true,
-        //        Message = "Game started"
-        //    };
-        //    //return $"{id}";
-        //    return new JsonResult(ro);
-        //}
-
-        // GET: GameController/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         [HttpPost("NewGame")]
         //[ValidateAntiForgeryToken]
         public ActionResult NewGame([FromBody] GameSettings s)
         {   
-            //_manager.SetGame(s.Game, (bool)s.Machine);
             return new JsonResult(_manager.StartGame(s.Game, (bool)s.Machine));
         }
 
-        // POST: GameController/Create
-        [HttpPost("Post")]
+        [HttpPost("TakeTurn")]
         //[ValidateAntiForgeryToken]
         public ActionResult Post([FromBody] GameInput item)
-        {
-            //try
-            //{
-            //    return RedirectToAction(nameof(Index));
-            //}
-            //catch
-            //{
-            //    return View();
-            //}
-
-            
+        {   
             return new JsonResult(_manager.TakeTurn(item.Game,item.Move));
         }
 
-        //// GET: GameController/Edit/5
-        //public ActionResult Edit(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: GameController/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
-
-        //// GET: GameController/Delete/5
-        //public ActionResult Delete(int id)
-        //{
-        //    return View();
-        //}
-
-        //// POST: GameController/Delete/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Delete(int id, IFormCollection collection)
-        //{
-        //    try
-        //    {
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    catch
-        //    {
-        //        return View();
-        //    }
-        //}
     }
 }
